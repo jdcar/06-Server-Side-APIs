809e735d353d1695b6904beac816974e7e01abc5
jdcar <jdcarlstone@gmail.com>
2020-12-12 21:03:05 -0600

    render current weather to DOM

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ var state = "Illinois";
 $("#searchButton").on("click", function (event) {
 
     event.preventDefault();
+    // grab search inputs for city and state
     var cityInput = $('#city-input').val()
     var stateInput = $('#state-input').val()
 
@@ -36,11 +37,44 @@ $("#searchButton").on("click", function (event) {
             url: queryURLwithState,
             method: "GET"
         }).then(function (response) {
+            console.log(response.wind.speed)
+            // temperture
+            var temp = response.main.temp
+            $('#temperature').append(temp + " degrees")
 
-            getWeather(response);
-         
+            // humidity
+            var humidity = response.main.humidity
+            $('#humidity').append(humidity + "%")
+            // wind 
+            var wind = response.wind.speed
+            $('#wind-speed').append(wind + " mph")
+            // latitude
+            var lat = response.coord.lat
+            // longitute
+            var lon = response.coord.lon
+
+            var queryForUv = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`
+
+            $.ajax({
+                url: queryForUv,
+                method: "GET"
+            }).then(function (response) {
+    
+                console.log(response.value)
+
+                var uvIndex = response.value
+                $('#uv-index').append(uvIndex)
+    
+                // uv index
+                // response. 
+    
+            });
+
+            
+
+
         });
-    } else if (cityInput != "" && stateInput === ""){
+    } else if (cityInput != "" && stateInput === "") {
 
         $.ajax({
             url: queryURLnoState,
@@ -49,7 +83,10 @@ $("#searchButton").on("click", function (event) {
 
             console.log(response)
 
-            getWeather(response);
+            // temperture
+            // humidity
+            // wind 
+            // uv index
 
         });
     }
@@ -57,3 +94,14 @@ $("#searchButton").on("click", function (event) {
 
 
 });
+
+// function getWeather () {
+
+//     console.log(reponse.)
+//             // temperture
+//             // humidity
+//             // wind 
+//             // uv index
+
+
+// }
